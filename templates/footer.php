bc6666c03af9a6425dd4527154502173b06e6250
systemshock89 <system.shock89@gmail.com>
2015-12-08 14:33:24 +0300

    metrica fix

--- templates/footer.php
+++ templates/footer.php
@@ -13,11 +13,20 @@
        </div>
        <a href="tel:+79607158265" class="phone animated">+7-960-715-82-65</a>
        <div class="counter">
-           <!-- Yandex.Metrika informer -->
-           <a href="https://metrika.yandex.ru/stat/?id=28213521&amp;from=informer"
-              target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/28213521/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-                                                  style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:28213521,lang:'ru'});return false}catch(e){}"/></a>
-           <!-- /Yandex.Metrika informer -->
+           <!-- Yandex.Metrika informer --><a href="https://metrika.yandex.ru/stat/?id=28213521&amp;from=informer" target="_blank" rel="nofollow"><img src="//informer.yandex.ru/informer/28213521/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:28213521,lang:'ru'});return false}catch(e){}"/></a><!-- /Yandex.Metrika informer --><!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter28213521 = new Ya.Metrika({id:28213521, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/28213521" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+
+           <!-- Google Analytics -->
+           <script>
+               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+               })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+               ga('create', 'UA-71174717-2', 'auto');
+               ga('send', 'pageview');
+
+           </script>
+           <!-- /Google Analytics -->
        </div>
        <div class="copyright animated">© <? echo date('Y') ?></div>
    </div>
@@ -25,23 +34,6 @@
 
 <!--preloader-->
 <!--<div class="page-preloader"><span class="spinner"></span></div>-->
-
-
-<!-- Google Analytics -->
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-71174717-2', 'auto');
-    ga('send', 'pageview');
-
-</script>
-<!-- /Google Analytics -->
-
-<!-- Yandex.Metrika informer --><a href="https://metrika.yandex.ru/stat/?id=28213521&amp;from=informer" target="_blank" rel="nofollow"><img src="//informer.yandex.ru/informer/28213521/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:28213521,lang:'ru'});return false}catch(e){}"/></a><!-- /Yandex.Metrika informer --><!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter28213521 = new Ya.Metrika({id:28213521, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/28213521" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
-
 
 <!-- scripts -->
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
